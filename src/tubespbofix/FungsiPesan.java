0dc41ce80a431144d289157c9c6c956cd513a130
Zedra19 <91422359+Zedra19@users.noreply.github.com>
2024-01-04 15:50:54 +0700

    Revert "Add Interface FungsiPesan"
    This reverts commit aaa3d05ae3fe9b8abe6b8ad1def3f04e43abb43e.

--- src/tubespbofix/FungsiPesan.java
+++
@@ -1,9 +0,0 @@
-
-package tubespbofix;
-
-public interface FungsiPesan {
-    public void insert_pesanan();
-    public void refresh_pesanan();
-    public void edit_pesanan();
-    public void delete_pesanan();
-}
