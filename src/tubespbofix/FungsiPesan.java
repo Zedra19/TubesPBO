38410aca8d006fbbfa1478e83fadb56fd95a3be1
Zedra19 <91422359+Zedra19@users.noreply.github.com>
2024-01-08 21:31:42 +0700

    Fixed FungsiPesan

--- src/tubespbofix/FungsiPesan.java
+++ src/tubespbofix/FungsiPesan.java
@@ -4,6 +4,4 @@ package tubespbofix;
 public interface FungsiPesan {
     public void insert_pesanan();
     public void edit_pesanan();
-    public void refresh_halaman();
-    public void delete_pesanan();
 }
